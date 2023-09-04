503b38f15d064344b3317782bd1b60c024be7bca
EsaTegarPutra <139981385+EsaTegarPutra@users.noreply.github.com>
2023-09-04 10:19:12 +0700

    Delete ranking_siswa_tailwind directory

--- ranking_siswa_tailwind/Layout/footer.php
+++
@@ -1,7 +0,0 @@
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#3b82f6" fill-opacity="1" d="M0,64L48,80C96,96,192,128,288,128C384,128,480,96,576,90.7C672,85,768,107,864,122.7C960,139,1056,149,1152,133.3C1248,117,1344,75,1392,53.3L1440,32L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
-    <!--footer-->
-    <footer class="bg-blue-500 text-white text-center pb-8">
-      <p>&copy; Copyright by <a href="" class="text-white font-bold">Esa Tegar Putra Utama</a></p>
-    </footer>
-</body>
-</html>
--- ranking_siswa_tailwind/Layout/header.php
+++
@@ -1,8 +0,0 @@
-<body>
-<nav class="bg-white top-0 w-full items-center z-1 sticky shadow-xl">
-    <div class="flex items-center justify-between relative mx-5">
-        <div class="px-4">
-            <a href="#" class="font-bold text-3xl block py-4">Student <span class="text-blue-500 font-bold text-3xl block">Ranks</span></a>
-        </div>
-    </div>    
-</nav>
